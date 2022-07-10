eeeedb7e445684133914ad9ca3d14221cbc65b3b
3TUSK <urey.s.knowledge@gmail.com>
2022-07-10 12:34:25 -0700

    Populates area id, name and properties into LP context

--- src/main/java/org/teacon/areacontrol/compat/luckperm/AreaControlContextCalculator.java
+++ src/main/java/org/teacon/areacontrol/compat/luckperm/AreaControlContextCalculator.java
@@ -6,12 +6,16 @@ import net.minecraft.server.level.ServerPlayer;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.teacon.areacontrol.AreaManager;
 
+import java.util.Objects;
+
 public enum AreaControlContextCalculator implements ContextCalculator<ServerPlayer> {
 
     INSTANCE;
 
-    private static final String CONTEXT_OWNER = "area_control.owning_current_position";
-    private static final String CONTEXT_ALLY = "area_control.ally_of_current_position";
+    private static final String CONTEXT_AREA_ID = "area_control:current_area_id";
+    private static final String CONTEXT_AREA_NAME = "area_control:current_area_name";
+    private static final String CONTEXT_OWNER = "area_control:owning_current_position";
+    private static final String CONTEXT_ALLY = "area_control:ally_of_current_position";
 
     @Override
     public void calculate(@NonNull ServerPlayer target, @NonNull ContextConsumer consumer) {
@@ -19,10 +23,14 @@ public enum AreaControlContextCalculator implements ContextCalculator<ServerPlay
         var pos = target.blockPosition();
         var dim = target.level.dimension();
         var area = AreaManager.INSTANCE.findBy(dim, pos);
+        consumer.accept(CONTEXT_AREA_ID, area.uid.toString());
+        consumer.accept(CONTEXT_AREA_NAME, area.name);
         if (area.owner != null) {
             consumer.accept(CONTEXT_OWNER, Boolean.toString(uid.equals(area.owner)));
         }
         consumer.accept(CONTEXT_ALLY, Boolean.toString(area.friends.contains(uid) || uid.equals(area.owner)));
-        // TODO Add all of our things into LuckPerm context, gg
+        for (var prop : area.properties.entrySet()) {
+            consumer.accept("area_control:property/" + prop.getKey(), Objects.toString(prop.getValue().toString()));
+        }
     }
 }
