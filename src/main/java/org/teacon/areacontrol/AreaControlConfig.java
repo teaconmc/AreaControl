6ad72d6a2e62b9be1ce3e2117f81506bd5e327cd
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2022-07-15 01:45:46 +0800

    Change default persistence mode to toml

--- src/main/java/org/teacon/areacontrol/AreaControlConfig.java
+++ src/main/java/org/teacon/areacontrol/AreaControlConfig.java
@@ -11,7 +11,7 @@ public class AreaControlConfig {
     public static ForgeConfigSpec setup(ForgeConfigSpec.Builder configSpec) {
         persistenceMode = configSpec.comment("The format in which the area data are stored. Currently supports json and toml.")
                 .translation("area_control.config.persistence_mode")
-                .define("persistenceMode", "json");
+                .define("persistenceMode", "toml");
         areaClaimTool = configSpec.comment("The item id of the item that should be used when marking areas for claiming. For example: minecraft:stick.")
                 .translation("area_control.config.area_claim_tool")
                 .define("areaClaimTool", "minecraft:stick");
