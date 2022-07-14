14b985c30f5d860634bc5154c48d5f02ca832fdd
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2022-07-15 02:16:08 +0800

    Revert "Switch to ConcurrentHashMap to support parallel access in scenarios such as LP async context calculation"
    This reverts commit 09efac19cf7e74ac5d05aea6171db189d27c40e0.

--- src/main/java/org/teacon/areacontrol/AreaManager.java
+++ src/main/java/org/teacon/areacontrol/AreaManager.java
@@ -2,12 +2,13 @@ package org.teacon.areacontrol;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.Nonnull;
 
@@ -41,24 +42,23 @@ public final class AreaManager {
 
     private AreaRepository repository = null;
 
-    private final Map<UUID, Area> areasById = new ConcurrentHashMap<>();
-    private final Map<String, Area> areasByName = new ConcurrentHashMap<>();
+    private final HashMap<UUID, Area> areasById = new HashMap<>();
+    private final HashMap<String, Area> areasByName = new HashMap<>();
     /**
      * All known instances of {@link Area}, indexed by dimensions and chunk positions covered by this area.
      * Used for faster lookup of {@link Area} when the position is known.
      */
-    private final Map<ResourceKey<Level>, Map<ChunkPos, Set<Area>>> perWorldAreaCache = new ConcurrentHashMap<>();
-
+    private final IdentityHashMap<ResourceKey<Level>, Map<ChunkPos, Set<Area>>> perWorldAreaCache = new IdentityHashMap<>();
     /**
      * All known instances of {@link Area}, indexed by dimensions only. Used for situations such as querying
      * areas in a larger range.
      */
-    private final Map<ResourceKey<Level>, Set<Area>> areasByWorld = new ConcurrentHashMap<>();
+    private final IdentityHashMap<ResourceKey<Level>, Set<Area>> areasByWorld = new IdentityHashMap<>();
     /**
      * All instances of "wildness" area, indexed by dimension.
      * They all have owner as {@link Area#GLOBAL_AREA_OWNER}.
      */
-    private final Map<ResourceKey<Level>, Area> wildnessByWorld = new ConcurrentHashMap<>();
+    private final IdentityHashMap<ResourceKey<Level>, Area> wildnessByWorld = new IdentityHashMap<>();
 
     private void buildCacheFor(Area area, ResourceKey<Level> worldIndex) {
         this.areasById.put(area.uid, area);
@@ -74,9 +74,9 @@ public final class AreaManager {
             this.wildnessByWorld.put(worldIndex, area);
             return;
         }
-        final Map<ChunkPos, Set<Area>> areasInDim = this.perWorldAreaCache.computeIfAbsent(worldIndex, id -> new ConcurrentHashMap<>());
+        final Map<ChunkPos, Set<Area>> areasInDim = this.perWorldAreaCache.computeIfAbsent(worldIndex, id -> new HashMap<>());
         ChunkPos.rangeClosed(new ChunkPos(area.minX >> 4, area.minZ >> 4), new ChunkPos(area.maxX >> 4, area.maxZ >> 4))
-                .map(cp -> areasInDim.computeIfAbsent(cp, _cp -> ConcurrentHashMap.newKeySet()))
+                .map(cp -> areasInDim.computeIfAbsent(cp, _cp -> Collections.newSetFromMap(new IdentityHashMap<>())))
                 .forEach(list -> list.add(area));
     }
 
