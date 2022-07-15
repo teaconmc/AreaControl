d22f89475e8e720a647afa3e30aaec49591f66b1
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2022-07-15 12:11:15 +0800

    Separate claims to different toml files

--- src/main/java/org/teacon/areacontrol/impl/persistence/TomlBasedAreaRepository.java
+++ src/main/java/org/teacon/areacontrol/impl/persistence/TomlBasedAreaRepository.java
@@ -7,14 +7,18 @@ import com.electronwill.nightconfig.core.conversion.Converter;
 import com.electronwill.nightconfig.core.conversion.ObjectConverter;
 import com.electronwill.nightconfig.core.conversion.SpecNotNull;
 import com.electronwill.nightconfig.core.file.FileConfig;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.io.MoreFiles;
+import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArraySet;
+import net.minecraft.core.BlockPos;
 import org.teacon.areacontrol.api.Area;
 
+import javax.annotation.Nullable;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TomlBasedAreaRepository implements AreaRepository {
 
@@ -26,89 +30,106 @@ public class TomlBasedAreaRepository implements AreaRepository {
 
     @Override
     public Collection<Area> load() throws Exception {
-        try (FileConfig areasData = FileConfig.of(this.dataDirRoot.resolve("claims.toml"))) {
-            areasData.load();
-            var converter = new ObjectConverter();
-            var model = converter.toObject(areasData, AreaRepoModel::new);
-            return model.toRealAreas();
+        var areas = new TreeMap<UUID, Area>();
+        @Nullable var exception = (IOException) null;
+        // noinspection UnstableApiUsage
+        for (Path file : MoreFiles.listFiles(this.dataDirRoot)) {
+            var fileName = file.getFileName();
+            if (fileName.startsWith("claim-") && fileName.endsWith(".toml")) {
+                try (FileConfig areasData = FileConfig.of(file)) {
+                    areasData.load();
+                    var converter = new ObjectConverter();
+                    var model = converter.toObject(areasData, AreaModel::new);
+                    Preconditions.checkArgument(Path.of("claim-%s.toml".formatted(model.uid)).equals(fileName),
+                            "The name of the claim file (" + fileName + ") does not match the claim uid: " + model.uid);
+                    areas.put(model.uid, model.toRealArea());
+                } catch (Exception e) {
+                    if (exception == null) {
+                        exception = new IOException("Failed to load one or more claim file(s)");
+                    }
+                    exception.addSuppressed(e);
+                }
+            }
         }
+        if (exception != null) {
+            throw exception;
+        }
+        return Collections.unmodifiableCollection(areas.values());
     }
 
     @Override
     public void save(Collection<Area> areas) throws Exception {
-        var model = new AreaRepoModel(areas);
-        try (FileConfig areasData = FileConfig.of(this.dataDirRoot.resolve("claims.toml"))) {
-            var converter = new ObjectConverter();
-            converter.toConfig(model, areasData);
-            areasData.save();
+        @Nullable var exception = (IOException) null;
+        for (Area area : areas) {
+            var model = new AreaModel(area);
+            try (FileConfig areasData = FileConfig.of(this.dataDirRoot.resolve("claims-%s.toml".formatted(area.uid)))) {
+                var converter = new ObjectConverter();
+                converter.toConfig(model, areasData);
+                areasData.save();
+            } catch (Exception e) {
+                if (exception == null) {
+                    exception = new IOException("Failed to save one or more claim file(s)");
+                }
+                exception.addSuppressed(e);
+            }
+        }
+        if (exception != null) {
+            throw exception;
         }
     }
 
-    private static final class AreaRepoModel {
-        public List<AreaModel> areas;
+    public static final class AreaModel {
+        @Conversion(UUIDConverter.class)
+        @SpecNotNull
+        public UUID uid;
+        @SpecNotNull
+        public String name;
+        @SpecNotNull
+        public String dimension = "minecraft:overworld";
+        @Conversion(UUIDConverter.class)
+        @SpecNotNull
+        public UUID owner = new UUID(0L, 0L);
+        @Conversion(UUIDCollectionConverter.class)
+        public Collection<UUID> friends = new HashSet<>();
 
-        public AreaRepoModel() {}
+        public int minX, minY, minZ, maxX, maxY, maxZ;
 
-        public AreaRepoModel(Collection<Area> realAreas) {
-            this.areas = realAreas.stream().map(AreaModel::new).toList();
+        public Config properties;
+
+        public AreaModel() {
         }
 
-        public List<Area> toRealAreas() {
-            return this.areas.stream().map(AreaModel::toRealArea).toList();
+        public AreaModel(Area realArea) {
+            this.uid = realArea.uid;
+            this.name = realArea.name;
+            this.dimension = realArea.dimension;
+            this.owner = realArea.owner;
+            this.friends = realArea.friends;
+            this.minX = realArea.minX;
+            this.minY = realArea.minY;
+            this.minZ = realArea.minZ;
+            this.maxX = realArea.maxX;
+            this.maxY = realArea.maxY;
+            this.maxZ = realArea.maxZ;
+            this.properties = Config.wrap(realArea.properties, InMemoryFormat.defaultInstance());
         }
 
-        public static final class AreaModel {
-            @Conversion(UUIDConverter.class)
-            @SpecNotNull
-            public UUID uid;
-            @SpecNotNull
-            public String name;
-            @SpecNotNull
-            public String dimension = "minecraft:overworld";
-            @Conversion(UUIDConverter.class)
-            @SpecNotNull
-            public UUID owner = new UUID(0L, 0L);
-            @Conversion(UUIDCollectionConverter.class)
-            public Collection<UUID> friends = new HashSet<>();
-
-            public int minX, minY, minZ, maxX, maxY, maxZ;
-
-            public Config properties;
-
-            public AreaModel() {}
-
-            public AreaModel(Area realArea) {
-                this.uid = realArea.uid;
-                this.name = realArea.name;
-                this.dimension = realArea.dimension;
-                this.owner = realArea.owner;
-                this.friends = realArea.friends;
-                this.minX = realArea.minX;
-                this.minY = realArea.minY;
-                this.minZ = realArea.minZ;
-                this.maxX = realArea.maxX;
-                this.maxY = realArea.maxY;
-                this.maxZ = realArea.maxZ;
-                this.properties = Config.wrap(realArea.properties, InMemoryFormat.defaultInstance());
-            }
-
-            public Area toRealArea() {
-                var area = new Area();
-                area.uid = this.uid;
-                area.name = this.name;
-                area.dimension = this.dimension;
-                area.owner = this.owner;
-                area.friends = new ObjectArraySet<>(this.friends);
-                area.minX = this.minX;
-                area.minY = this.minY;
-                area.minZ = this.minZ;
-                area.maxX = this.maxX;
-                area.maxY = this.maxY;
-                area.maxZ = this.maxZ;
-                area.properties.clear();
-                area.properties.putAll(this.properties.valueMap());
-                return area;
-            }
+        public Area toRealArea() {
+            var area = new Area();
+            area.uid = this.uid;
+            area.name = this.name;
+            area.dimension = this.dimension;
+            area.owner = this.owner;
+            area.friends = new ObjectArraySet<>(this.friends);
+            area.minX = this.minX;
+            area.minY = this.minY;
+            area.minZ = this.minZ;
+            area.maxX = this.maxX;
+            area.maxY = this.maxY;
+            area.maxZ = this.maxZ;
+            area.properties.clear();
+            area.properties.putAll(this.properties.valueMap());
+            return area;
         }
     }
 
