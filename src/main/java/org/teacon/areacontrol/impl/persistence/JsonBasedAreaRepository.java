02f24434eedad11d271e8c67b0990d569fd54c1a
3TUSK <urey.s.knowledge@gmail.com>
2022-07-16 12:03:21 -0700

    Fix JsonBasedAreaRepository

--- src/main/java/org/teacon/areacontrol/impl/persistence/JsonBasedAreaRepository.java
+++ src/main/java/org/teacon/areacontrol/impl/persistence/JsonBasedAreaRepository.java
@@ -1,15 +1,20 @@
 package org.teacon.areacontrol.impl.persistence;
 
+import com.google.common.base.Preconditions;
+import com.google.common.io.MoreFiles;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.teacon.areacontrol.api.Area;
 
+import javax.annotation.Nullable;
+import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.TreeMap;
 import java.util.UUID;
 
 public class JsonBasedAreaRepository implements AreaRepository {
@@ -23,14 +28,36 @@ public class JsonBasedAreaRepository implements AreaRepository {
 
     @Override
     public Collection<Area> load() throws Exception {
-        var areas = new ArrayList<Area>();
-        Path userDefinedAreas = dataDirRoot.resolve("claims.json");
-        if (Files.isRegularFile(userDefinedAreas)) {
-            try (Reader reader = Files.newBufferedReader(userDefinedAreas)) {
-                areas.addAll(Arrays.asList(GSON.fromJson(reader, Area[].class)));
+        var areas = new TreeMap<UUID, Area>();
+        @Nullable var exception = (IOException) null;
+        // noinspection UnstableApiUsage
+        for (Path file : MoreFiles.listFiles(this.dataDirRoot)) {
+            var fileName = file.getFileName().toString();
+            if (fileName.startsWith("claim-") && fileName.endsWith(".json")) {
+                try (Reader reader = Files.newBufferedReader(file)) {
+                    var model = GSON.fromJson(reader, Area.class);
+                    Preconditions.checkArgument("claim-%s.json".formatted(model.uid).equals(fileName),
+                            "The name of the claim file (" + fileName + ") does not match the claim uid: " + model.uid);
+                    areas.put(model.uid, model);
+                } catch (Exception e) {
+                    if (exception == null) {
+                        exception = new IOException("Failed to load one or more claim file(s)");
+                    }
+                    exception.addSuppressed(e);
+                }
             }
         }
+        if (exception != null) {
+            throw exception;
+        }
+        var ret = new ArrayList<>(areas.values());
         // Check for presence of legacy data for auto-migration
+        Path legacyAreas = dataDirRoot.resolve("claims.json");
+        if (Files.isRegularFile(legacyAreas)) {
+            try (Reader reader = Files.newBufferedReader(legacyAreas)) {
+                ret.addAll(Arrays.asList(GSON.fromJson(reader, Area[].class)));
+            }
+        }
         Path oldWildness = dataDirRoot.resolve("wildness.json");
         if (Files.isRegularFile(oldWildness)) {
             boolean success = false;
@@ -38,18 +65,31 @@ public class JsonBasedAreaRepository implements AreaRepository {
                 Area wildness = GSON.fromJson(reader, Area.class);
                 wildness.uid = UUID.randomUUID();
                 wildness.owner = Area.GLOBAL_AREA_OWNER;
-                success = areas.add(wildness);
+                success = ret.add(wildness);
             }
             if (success) {
                 Files.deleteIfExists(oldWildness);
             }
         }
-        return areas;
+        return ret;
     }
 
     @Override
     public void save(Collection<Area> areas) throws Exception {
-        Files.writeString(dataDirRoot.resolve("claims.json"), GSON.toJson(areas));
+        @Nullable var exception = (IOException) null;
+        for (Area area : areas) {
+            try {
+                Files.writeString(this.dataDirRoot.resolve("claim-%s.json".formatted(area.uid)), GSON.toJson(area));
+            } catch (Exception e) {
+                if (exception == null) {
+                    exception = new IOException("Failed to save one or more claim file(s)");
+                }
+                exception.addSuppressed(e);
+            }
+        }
+        if (exception != null) {
+            throw exception;
+        }
     }
 
 }
