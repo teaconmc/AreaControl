7224b2916cbfe7fd5d98f3c0da4f1fabb8516dbd
USS-Shenzhou <969840795@qq.com>
2022-07-28 12:15:58 +0800

    When allowFireSpread is not specified, use doFireTick instead.

--- src/main/java/org/teacon/areacontrol/mixin/FireMixin.java
+++ src/main/java/org/teacon/areacontrol/mixin/FireMixin.java
@@ -2,16 +2,19 @@ package org.teacon.areacontrol.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.teacon.areacontrol.AreaManager;
 import org.teacon.areacontrol.api.Area;
 import org.teacon.areacontrol.api.AreaProperties;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Mixin(FireBlock.class)
@@ -21,17 +24,12 @@ public class FireMixin {
      * Prepend an AreaControl check to determine whether fire tick should be stopped.
      * Note that this can also stop rain from extinguishing fire. Manually destroying
      * fire blocks is not affected.
-     * @param state Captured BlockState
-     * @param level Captured ServerLevel
-     * @param pos Captured BlockPos
-     * @param rand Captured Random
-     * @param ci The callback
+     * If allowFireSpread is not specified in the area, continue to use doFireTick in gameRule instead.
      */
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;scheduleTick(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Block;I)V", shift = At.Shift.AFTER), cancellable = true)
-    private void fireSpreadCheck(BlockState state, ServerLevel level, BlockPos pos, Random rand, CallbackInfo ci) {
-        Area current = AreaManager.INSTANCE.findBy(level, pos);
-        if (!AreaProperties.getBool(current, "area.allow_fire_spread")) {
-            ci.cancel();
-        }
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z"))
+    private boolean fireSpreadCheck(GameRules instance, GameRules.Key<GameRules.BooleanValue> pKey, BlockState pState, ServerLevel pLevel, BlockPos pPos) {
+        Area area = AreaManager.INSTANCE.findBy(pLevel, pPos);
+        Optional<Boolean> allowFireSpread = AreaProperties.getBoolOptional(area, AreaProperties.ALLOW_FIRE_SPREAD);
+        return allowFireSpread.orElseGet(() -> pLevel.getGameRules().getBoolean(GameRules.RULE_DOFIRETICK));
     }
 }
--- src/main/java/org/teacon/areacontrol/mixin/LavaMixin.java
+++ src/main/java/org/teacon/areacontrol/mixin/LavaMixin.java
@@ -1,12 +1,14 @@
 package org.teacon.areacontrol.mixin;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.LavaFluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.teacon.areacontrol.AreaManager;
 import org.teacon.areacontrol.api.Area;
@@ -25,12 +27,10 @@ public class LavaMixin {
      * Prepend an AreaControl check to determine whether lava can perform random tick.
      * If allowFireSpread is not specified in the area, continue to use doFireTick in gameRule instead.
      */
-    @Inject(method = "randomTick", at = @At(value = "HEAD"), cancellable = true)
-    private void randomTickCheck(Level pLevel, BlockPos pPos, FluidState pState, Random pRandom, CallbackInfo ci) {
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z"))
+    private boolean randomTickCheck(GameRules instance, GameRules.Key<GameRules.BooleanValue> pKey, Level pLevel, BlockPos pPos) {
         Area area = AreaManager.INSTANCE.findBy(pLevel, pPos);
         Optional<Boolean> allowFireSpread = AreaProperties.getBoolOptional(area, AreaProperties.ALLOW_FIRE_SPREAD);
-        if (allowFireSpread.isPresent() && !allowFireSpread.get()) {
-            ci.cancel();
-        }
+        return allowFireSpread.orElseGet(() -> pLevel.getGameRules().getBoolean(GameRules.RULE_DOFIRETICK));
     }
 }
