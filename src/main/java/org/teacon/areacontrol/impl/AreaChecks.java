a5b8b75450d1cccd6c5f39f96e788d59e0bec5c2
3TUSK <urey.s.knowledge@gmail.com>
2022-08-08 20:48:19 -0700

    Simply code

--- src/main/java/org/teacon/areacontrol/impl/AreaChecks.java
+++ src/main/java/org/teacon/areacontrol/impl/AreaChecks.java
@@ -34,7 +34,7 @@ public class AreaChecks {
         var invSize = inv.size();
         for (int i = 0; i < invSize; i++) {
             var item = inv.get(i);
-            if (!item.isEmpty() && !checkPossess(currentArea, AreaProperties.ALLOW_POSSESS, item.getItem())) {
+            if (!item.isEmpty() && !checkPossess(currentArea, item.getItem())) {
                 inv.set(i, ItemStack.EMPTY);
                 player.displayClientMessage(new TranslatableComponent("area_control.notice.possess_disabled_item", item.getHoverName()), true);
             }
@@ -42,14 +42,14 @@ public class AreaChecks {
     }
 
     // This is a separate method because area.allow_possess currently has a different logic
-    private static boolean checkPossess(Area area, String prop, Item item) {
+    private static boolean checkPossess(Area area, Item item) {
         var targetId = item.getRegistryName();
         if (targetId != null) {
-            var objSpecific = AreaProperties.getBoolOptional(area, prop + "." + targetId);
+            var objSpecific = AreaProperties.getBoolOptional(area, AreaProperties.ALLOW_POSSESS + "." + targetId);
             if (objSpecific.isPresent()) {
                 return objSpecific.get();
             } else {
-                var modSpecific = AreaProperties.getBoolOptional(area, prop + "." + targetId.getNamespace());
+                var modSpecific = AreaProperties.getBoolOptional(area, AreaProperties.ALLOW_POSSESS + "." + targetId.getNamespace());
                 if (modSpecific.isPresent()) {
                     return modSpecific.get();
                 }
