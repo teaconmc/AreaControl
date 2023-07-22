6daadb358d83e63ae2cb7059716aad61ef7b5c52
3TUSK <git@tritusk.info>
2023-07-21 23:01:07 -0700

    EntitySelector selects entities within the same area as of command executor by default

---
+++ src/main/java/org/teacon/areacontrol/mixin/EntitySelectorMixin.java
@@ -0,0 +1,34 @@
+package org.teacon.areacontrol.mixin;
+
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.selector.EntitySelector;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.teacon.areacontrol.AreaManager;
+
+@Mixin(EntitySelector.class)
+public class EntitySelectorMixin {
+
+    /**
+     * Modify the filter to include area check: if the entity in question is not in the area that
+     * command source locates at, then it will be excluded.
+     * @param sourceStack Command initiator
+     * @param e Entity in question
+     * @param cir callback
+     */
+    @Inject(method = "lambda$findEntities$1", at = @At("HEAD"), cancellable = true)
+    private static void checkArea(CommandSourceStack sourceStack, Entity e, CallbackInfoReturnable<Boolean> cir) {
+        var level = sourceStack.getLevel();
+        var pos = sourceStack.getPosition();
+        var area = AreaManager.INSTANCE.findBy(level, pos);
+        if (area != null) {
+            if (e.xo < area.minX || e.xo > area.maxX || e.yo < area.minY || e.yo > area.maxY || e.zo < area.minZ || e.zo > area.maxZ) {
+                cir.setReturnValue(Boolean.FALSE);
+            }
+        }
+    }
+
+}
