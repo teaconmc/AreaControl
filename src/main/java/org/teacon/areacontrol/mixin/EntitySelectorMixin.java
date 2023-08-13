5e8b4f66be17e3bed1fdc8e668eeab78160f884f
3TUSK <git@tritusk.info>
2023-08-12 21:27:11 -0700

    0.7.13: players can now receive a notification on how many entities are excluded due to ACtrl
    Very cursed.

--- src/main/java/org/teacon/areacontrol/mixin/EntitySelectorMixin.java
+++ src/main/java/org/teacon/areacontrol/mixin/EntitySelectorMixin.java
@@ -2,29 +2,47 @@ package org.teacon.areacontrol.mixin;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.selector.EntitySelector;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.teacon.areacontrol.impl.AreaEntitySelectorChecker;
 
+import java.util.List;
+
 @Mixin(EntitySelector.class)
-public class EntitySelectorMixin {
+public abstract class EntitySelectorMixin {
 
     /**
-     * Modify the filter to include area check: if the entity in question is not in the area that
-     * command source locates at, then it will be excluded.
+     * Modify check to exclude entities that failed area check, and notify selector initiator on
+     * how many entities were excluded.
      * @param sourceStack Command initiator
-     * @param e Entity in question
      * @param cir callback
      */
-    @Inject(method = { "lambda$findEntities$1", "m_244752_" }, at = @At("HEAD"),
-            cancellable = true, remap = false, require = 1)
-    private static void checkArea(CommandSourceStack sourceStack, Entity e, CallbackInfoReturnable<Boolean> cir) {
-        if (!AreaEntitySelectorChecker.check(sourceStack, e)) {
-            cir.setReturnValue(Boolean.FALSE);
+    // TODO This is an example use-case of @ModifyReturnValue from MixinExtras, which we don't have.
+    @Inject(method = "findEntities", at = @At("HEAD"), cancellable = true)
+    private void checkArea(CommandSourceStack sourceStack, CallbackInfoReturnable<List<? extends Entity>> cir) {
+        var original = this.findEntitiesRaw(sourceStack);
+        var itr = original.iterator();
+        int removedDueToACtrl = 0;
+        while (itr.hasNext()) {
+            var e = itr.next();
+            if (!e.getType().isEnabled(sourceStack.enabledFeatures())) {
+                itr.remove();
+            } else if (!AreaEntitySelectorChecker.check(sourceStack, e)) {
+                itr.remove();
+                removedDueToACtrl++;
+            }
         }
+        final String formattedCount = Integer.toString(removedDueToACtrl);
+        sourceStack.sendSuccess(() -> Component.translatable("area_control.notice.selector_filtered", formattedCount), true);
+        cir.setReturnValue(original);
     }
+
+    @Shadow
+    protected abstract List<? extends Entity> findEntitiesRaw(CommandSourceStack sourceStack);
 
 }
