4db842ad5d6a6dd8e0e34d7f3a793db60107d262
3TUSK <git@tritusk.info>
2024-08-14 13:56:20 -0700

    NeoForge's new damage pipeline supersedes one of our mixin usage

--- src/main/java/org/teacon/areacontrol/AreaControlEventHandlers.java
+++ src/main/java/org/teacon/areacontrol/AreaControlEventHandlers.java
@@ -5,10 +5,15 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityInvulnerabilityCheckEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.EntityMountEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
@@ -20,6 +25,53 @@ import org.teacon.areacontrol.impl.AreaChecks;
 
 @EventBusSubscriber(modid = "area_control")
 public final class AreaControlEventHandlers {
+
+    /*
+     * This is probably one of the most universal ways to prevent edge cases such as
+     * preventing an arrow from unauthorized player to shoot down vanilla item frame.
+     * Vanilla Item Frames are not LivingEntity so LivingAttackEvent cannot catch this
+     * case.
+     * When arrows hit vanilla Item Frames, the "damage" does not list Player as
+     * direct source (lists as indirect source, a.k.a. "true source"), so
+     * AttackEntityEvent cannot catch this case, either.
+     * Use Entity.hurt as an injecting target has an issue: anyone who extends from
+     * Entity can override that method and make our injection in vain, and thus requires
+     * us to special-casing every single edge case.
+     * All of these leave isInvulnerableTo a most-probable choice. If your entity can
+     * subject to some form of attack, you most likely will need to call this method.
+     * This is the case for vanilla entities.
+     *
+     * NeoForge update: NeoForge has an event at this spot, so we no longer need to mix-in
+     * on our own. - 3TUSK
+     */
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void vulnerabilityCheck(EntityInvulnerabilityCheckEvent event) {
+        DamageSource src = event.getSource();
+        Entity attackTarget = event.getEntity();
+        Level level = attackTarget.level();
+        boolean originalInvulnerability = event.getOriginalInvulnerability();
+        if (!src.is(DamageTypeTags.BYPASSES_INVULNERABILITY) && !originalInvulnerability && !level.isClientSide()) {
+            var area = AreaManager.INSTANCE.findBy(level, attackTarget.blockPosition());
+            boolean allow;
+            Component deniedFeedback;
+            var damageSrc = src.getEntity();
+            if (attackTarget instanceof Player && damageSrc instanceof Player) {
+                allow = AreaChecks.checkPropFor(area, damageSrc, AreaProperties.ALLOW_PVP, null, AreaControlConfig.allowPvP);
+                deniedFeedback = Component.translatable("area_control.notice.pvp_disabled", ObjectArrays.EMPTY_ARRAY);
+            } else {
+                var entityTypeRegName = BuiltInRegistries.ENTITY_TYPE.getKey(attackTarget.getType());
+                allow = AreaChecks.checkPropFor(area, damageSrc, AreaProperties.ALLOW_PVE, entityTypeRegName, AreaControlConfig.allowPvE);
+                deniedFeedback = Component.translatable("area_control.notice.pve_disabled", ObjectArrays.EMPTY_ARRAY);
+            }
+            if (!allow) {
+                if (src.getEntity() instanceof ServerPlayer srcPlayer) {
+                    srcPlayer.displayClientMessage(deniedFeedback, true);
+                }
+                event.setInvulnerable(true);
+            }
+        }
+    }
+
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onCheckSpawn(EntityJoinLevelEvent event) {
         // Note from 3TUSK:
