f31665bf182bc8b765ff93485ded82ed09555b45
3TUSK <git@tritusk.info>
2024-10-08 13:04:06 -0700

    0.8.10: limit entity selector filtering to selectors targeting > 1 non-player entities
    Exception: if an entity is selected by UUID, it won't be filtered.
    Exception: selectors that selecting players are no longer affected by filtering.

--- src/main/java/org/teacon/areacontrol/mixin/EntitySelectorMixin.java
+++ src/main/java/org/teacon/areacontrol/mixin/EntitySelectorMixin.java
@@ -1,10 +1,12 @@
 package org.teacon.areacontrol.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.selector.EntitySelector;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.teacon.areacontrol.impl.AreaEntitySelectorChecker;
@@ -19,10 +21,10 @@ public abstract class EntitySelectorMixin {
      * Modify check to exclude entities that failed area check, and notify selector initiator on
      * how many entities were excluded.
      */
-    @ModifyReturnValue(method = "findEntities", at = @At("RETURN"))
-    private List<? extends Entity> checkArea(List<? extends Entity> original, CommandSourceStack sourceStack) {
+    @WrapOperation(method = "findEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/arguments/selector/EntitySelector;sortAndLimit(Lnet/minecraft/world/phys/Vec3;Ljava/util/List;)Ljava/util/List;"))
+    private List<? extends Entity> checkArea(EntitySelector instance, Vec3 pos, List<? extends Entity> original, Operation<List<? extends Entity>> call, CommandSourceStack sourceStack) {
         var removedDueToACtrl = 0;
-        var filtered = new ArrayList<Entity>();
+        var filtered = new ArrayList<Entity>(original.size());
         for (var e : original) {
             if (AreaEntitySelectorChecker.check(sourceStack, e)) {
                 filtered.add(e);
@@ -34,6 +36,6 @@ public abstract class EntitySelectorMixin {
             final String formattedCount = Integer.toString(removedDueToACtrl);
             sourceStack.sendSuccess(() -> Component.translatable("area_control.notice.selector_filtered", formattedCount), true);
         }
-        return List.copyOf(filtered);
+        return call.call(instance, pos, filtered);
     }
 }
